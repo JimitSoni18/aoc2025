d4f26f9a962c8df093d5d8ddc2b8bcefb6486a3f
jimitsoni18 <jimitsoni18@gmail.com>
2025-12-05 08:43:39 +0530

    d2p2 started

--- src/day2.rs
+++ src/day2.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 const INPUT: &str = include_str!("../input/day-2.txt");
 
 pub fn part_1() -> usize {
@@ -5,15 +7,57 @@ pub fn part_1() -> usize {
 		.split(',')
 		.map(|range| {
 			let (start, end) = range.split_once('-').unwrap();
-			(start.parse::<usize>().unwrap()..=end.parse::<usize>().unwrap())
+			(start.parse::<usize>().unwrap()..=end.parse().unwrap())
 				.filter(|num| {
 					let num_str = num.to_string();
 					let len = num_str.len();
-					if len % 2 == 0 {
-						let (first, second) = num_str.split_at(len / 2);
-						return first == second;
+					if len % 2 != 0 {
+						return false;
 					}
-					false
+					let (first, second) = num_str.split_at(len / 2);
+					first == second
+				})
+				.sum::<usize>()
+		})
+		.sum::<usize>()
+}
+
+pub fn part_2() -> usize {
+	INPUT
+		.split(',')
+		.map(|range| {
+			let (start, end) = range.split_once('-').unwrap();
+			(start.parse::<usize>().unwrap()..=end.parse().unwrap())
+				.filter(|num| {
+					let num_str = num.to_string();
+                    let mut chars = num_str.chars();
+                    let first = chars.next().unwrap();
+
+                    let all_chars = HashSet::<char>::from_iter(chars);
+
+					let mut matched_indices: Vec<_> = num_str
+						.match_indices(first)
+						.map(|m| m.0)
+						.collect();
+
+					if matched_indices.len() == 1 {
+						return false;
+					}
+					if *matched_indices.last().unwrap() != num_str.len() {
+						matched_indices.push(num_str.len());
+					}
+
+                    // FIXME: does not work when the same digit is repeated in sequence, example:
+                    // 11851185
+                    // TRY: merging windows until the window has all the distinct characters that
+                    // the entire string has
+					for window in matched_indices.windows(3) {
+						if num_str[window[0]..window[1]] != num_str[window[1]..window[2]] {
+							return false;
+						}
+					}
+
+					true
 				})
 				.sum::<usize>()
 		})
--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,6 @@ fn main() {
 	println!("day 1 part 1: {}", day1::part_1());
 	println!("day 1 part 2: {}", day1::part_2());
 	println!("day 2 part 1: {}", day2::part_1());
+	println!("day 2 part 2: {}", day2::part_2());
 }
 
